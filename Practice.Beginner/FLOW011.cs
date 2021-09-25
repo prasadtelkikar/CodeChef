46879ced69671087a812bc05eef0e711a583a512
Prasad L Telkikar <telkikar.prasad@gmail.com>
2021-09-26 00:22:13 +0530

    Solved Gross Salary

---
+++ Practice.Beginner/FLOW011.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice.Beginner
+{
+    public class FLOW011
+    {
+        public static void Main(string[] args)
+        {
+            var testCases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < testCases; i++)
+            {
+                var basics = int.Parse(Console.ReadLine());
+                double sal = basics < 1500
+                    ? (basics + (0.1 * basics) + (0.9 * basics))
+                    : (basics + 500 + (0.98 * basics));
+                Console.WriteLine(sal);
+            }
+
+        }
+    }
+}

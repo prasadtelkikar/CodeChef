c5bf4016174bbfb4c231b106fecc49a3ec325323
Prasad L Telkikar <telkikar.prasad@gmail.com>
2021-09-25 23:44:14 +0530

    Solved Enormous Input Test practice problem

---
+++ Practice.Beginner/INTEST.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Practice.Beginner
+{
+    public class INTEST
+    {
+        public static void Main(string[] args)
+        {
+            var inputStr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            var n = inputStr[0];
+            var k = inputStr[1];
+
+            var count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                var number = int.Parse(Console.ReadLine());
+                if (number % k == 0)
+                    count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}

25b6dd34fef26e719df4e30afb442e4d94a7038a
prasadtelkikar <telkikar.prasad@gmail.com>
2021-10-08 00:38:24 +0530

    Sample project where debugger works fine.

---
+++ solution-ubuntu-1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace solution_ubuntu_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Console.ReadLine();
+        }
+    }
+}

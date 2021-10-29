428a7754ec6758ba522f769258919f9769687b29
prasadtelkikar <telkikar.prasad@gmail.com>
2021-10-29 22:24:31 +0530

    Solved CATSDOGS

--- solutions-ubuntu/CATSDOGS.cs
+++ solutions-ubuntu/CATSDOGS.cs
@@ -1,31 +1,49 @@
 using System;
-using System.Linq;
 using System.Collections.Generic;
 
 namespace solutions_ubuntu
 {
+    //Thanks to : https://youtu.be/N2e8wf1QcbM
     public class CATDOGS
     {
         public static void Main(string[] args)
         {
             var testCases = int.Parse(Console.ReadLine().Trim());
             var result = new List<string>();
-            for(var i = 0; i < testCases; i++)
+            for(int i = 0; i < testCases; i++)
             {
                 var inputs = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
-                var legCount = inputs[2];
-                var dogLegCount = inputs[1];
-                var inputCatLegCount = inputs[0];
-                var actualLegCount = dogLegCount * 4 + inputCatLegCount * 4;
-                if(actualLegCount <= legCount)
-                    result.Add("yes");
-                else
+                int catCount = inputs[0];
+                int dogCount = inputs[1];
+                int legCount = inputs[2];
+                if(legCount % 4 != 0)
                 {
-                    //Failed attempt of CATSDOGS
+                    result.Add("no");
+                    continue;
 
                 }
+                else
+                {
+                    var animalCount = legCount / 4;
+                    var catOnGround = animalCount - dogCount;
+                    if(catOnGround < 0 || catOnGround > catCount)
+                    {
+                        result.Add("no");
+                        continue;
+                    }
+                    if(catOnGround < catCount)
+                    {
+                        var catsOnDog = catCount - catOnGround;
+                        if(catsOnDog < 0 || catsOnDog > (2 * dogCount))
+                        {
+                            result.Add("no");
+                            continue;
+                        }
+                    }
+                }
+                result.Add("yes");
             }
-            Console.WriteLine(string.Join(Environment.NewLine, result));
+            Console.WriteLine(string.Join(Environment.NewLine,result));
         }
     }
 }

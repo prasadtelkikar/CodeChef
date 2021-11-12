7da0e7ee6e48a2d1bdc86ddf3abfb91053079d48
prasadtelkikar <telkikar.prasad@gmail.com>
2021-11-12 23:08:21 +0530

    Solved Rich chef problem

---
+++ Practice.Beginner/CHFRICH.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice.Beginner
+{
+    public class CHFRICH
+    {
+        public static void Main(string[] args)
+        {
+            var testCases = Convert.ToInt32(Console.ReadLine());
+            var result = new List<int>();
+            for (int i = 0; i < testCases; i++)
+            {
+                var inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                var output = (inputs[1] - inputs[0]) / inputs[2];
+                result.Add(output);
+            }
+            Console.WriteLine(String.Join(Environment.NewLine, result.ToArray()));
+        }
+    }
+}
